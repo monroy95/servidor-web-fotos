de87bb2cd82aa0335819dbccb6825faa015f68d6
monroy95 <m.monroy123ap@gmail.com>
2017-10-07 01:45:33 -0700

    Agregando Js en el cliente

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var babel = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 // Permite que por medio de otro pipe podamos renombrar el archivo .css que genere
 // Define una nueva tarea de estilos
 gulp.task('styles', function() {
@@ -19,4 +22,13 @@ gulp.task('assets', function() {
         .pipe(gulp.dest('public'));
 })
 
-gulp.task('default', ['styles', 'assets']) // El arrar puede ejecutar varias tareas en paralelo
+// Tarea: que procese index.js
+gulp.task('scripts', function() {
+    browserify('./src/index.js')
+        .transform(babel)
+        .bundle()
+        .pipe(source('index.js')) // Transforma lo que devuelve bundle 
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest('public'));
+})
+gulp.task('default', ['styles', 'assets', 'scripts']) // El arrar puede ejecutar varias tareas en paralelo
