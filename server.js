4f05531bfcc3aad7b493b06d1f9a2e952cf6acf6
monroy95 <m.monroy123ap@gmail.com>
2017-10-07 00:56:37 -0700

    Agregando vistas con pug

--- server.js
+++ server.js
@@ -3,18 +3,19 @@ var express = require('express');
 
 var app = express();
 
+// Indicando que se utilizara un motor de vistas "pug"
+app.set('view engine', 'pug');
 // Cuando corre el script server.js se ejecutara lo siguiente
 app.get('/', function(req, res) {
-    res.send('Hola el servidor web esta corriendo')
+    // res.send('Hola el servidor web esta corriendo')
+    res.render('index');
 })
 
 // El servidor web esuchara por medio del puerto 3000 y tiene como parametro un error
 // En caso exista un error el proceso terminara retornando el mensaje "Hubo un error"
 // De lo contrario mostrara el mensaje "Servidor Web escuchando en el puerto 3000"
 app.listen(3000, function(err) {
-    if (err) {
-        return console.log('Hubo un error'), process.exit(1);
+    if (err) return console.log('Hubo un error'), process.exit(1);
 
-        console.log('Servidor Web escuchando en el puerto 3000');
-    }
+    console.log('Servidor Web escuchando en el puerto 3000');
 })
